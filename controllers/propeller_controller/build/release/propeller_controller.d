93396bdfb80cc00a277099d5beb5190b79dce9d3
tsioftas <panaevri@gmail.com>
2021-01-29 12:00:53 -0300

    Deleted propeler controller (unused)

--- controllers/propeller_controller/build/release/propeller_controller.d
+++
@@ -1,8 +0,0 @@
-build/release/propeller_controller.o: propeller_controller.c \
- C:\Users\konto\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
- C:\Users\konto\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
- C:\Users\konto\AppData\Local\Programs\Webots/include/controller/c/webots/inertial_unit.h \
- C:\Users\konto\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
- C:\Users\konto\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
- C:\Users\konto\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
- C:\Users\konto\AppData\Local\Programs\Webots/include/controller/c/webots/supervisor.h
